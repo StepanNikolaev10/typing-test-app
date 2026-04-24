95b601b3da4bc60173c405977c23cc35e97fc252
NikolaS <nikolaevs1006@gmail.com>
2026-04-24 18:05:53 +0300

    feat(root): add routing and theme changeability features

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router";
+import StartPage from "./pages/StartPage/StartPage";
+import styles from "./App.module.scss";
+import TypingPage from "./pages/TypingPage/TypingPage";
+import { useEffect, useState } from "react";
+import type { TTheme } from "./types/theme";
 
 function App() {
+  const [theme, setTheme] = useState<TTheme>("light");
+
+  // получение темы из localStorage
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (!storedTheme) return;
+    const theme = JSON.parse(storedTheme);
+    setTheme(theme);
+  }, []);
+
+  const changeTheme = () => {
+    let changedTheme: TTheme;
+    theme === "light" ? (changedTheme = "dark") : (changedTheme = "light");
+    localStorage.setItem("theme", JSON.stringify(changedTheme));
+    setTheme(changedTheme);
+  };
+
   return (
-    <>
-
-    </>
-  )
+    <BrowserRouter>
+      <button className={styles.toggleThemeBtn} onClick={changeTheme}></button>
+      <Routes>
+        <Route path="/" element={<StartPage theme={theme} />} />
+        <Route path="/typing" element={<TypingPage theme={theme} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
